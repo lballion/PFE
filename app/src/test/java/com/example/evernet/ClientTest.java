0977e47e1fa76ec5daa18eb9d3f518cdec2e42a5
Sarah Portejoie <vasawin09@gmail.com>
2021-03-16 22:06:23 +0100

    Add tests and clean Client.java

--- app/src/test/java/com/example/evernet/ClientTest.java
+++ app/src/test/java/com/example/evernet/ClientTest.java
@@ -2,16 +2,18 @@ package com.example.evernet;
 
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
-public class ExampleUnitTest {
+public class ClientTest {
+
     @Test
-    public void addition_isCorrect() {
-        assertEquals(4, 2 + 2);
+    public void openSocketTest () {
+
+        assertEquals(1, 1);
     }
 }
