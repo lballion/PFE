eec1138d4d0c7cbf854652f9f88d1b2fa0f4e7e4
thierno <ts76gabalan@gmail.com>
2021-03-19 15:46:26 +0100

    Test illegal values in Packet class

--- app/src/main/java/com/domain/evernet/model/Packet.java
+++ app/src/main/java/com/domain/evernet/model/Packet.java
@@ -12,8 +12,22 @@ public class Packet {
 
     public Packet(String src, String dst, int pos, int nbPackets, int ttl, String timeStmp, String imgFrag) {
 
-        if( src.equals("") || dst.equals("") || pos < 0 || nbPackets < 0 || imgFrag.equals("") || ttl < 0){
-            throw new IllegalArgumentException("Args must be positive integers.");
+
+        boolean maxValues = pos <= 9999 &&  nbPackets <= 9999 && ttl <= 9;
+        boolean minValues = pos >= 0 && nbPackets >= 0 && ttl >= 0;
+
+        if( ! minValues || ! maxValues) {
+            throw new IllegalArgumentException("Args must be positive integers  and includes between 0 and 9999.");
+        }
+
+        if(src == null || dst== null || timeStmp == null || imgFrag == null) {
+            throw new IllegalArgumentException("Strings must not be null.");
+        }
+
+        boolean containsEmptyValues = src.equals("") || dst.equals("") || imgFrag.equals("");
+        boolean maxSizeExceeded = src.length() >= 10 || dst.length() >= 10 || timeStmp.length() != 6 ;
+        if(containsEmptyValues  || maxSizeExceeded) {
+            throw new IllegalArgumentException("Max size exceeded.");
         }
         source = src;
         destination = dst;
