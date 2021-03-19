f788e98ccd3bdbbf993b33a317287e6e474a91ed
lballion <lucasballion@hotmail.fr>
2021-03-19 19:20:50 +0100

    Modification de la class Contact afin qu'elle soit Parcelable

--- app/src/main/java/com/domain/evernet/model/Contact.java
+++ app/src/main/java/com/domain/evernet/model/Contact.java
@@ -1,8 +1,11 @@
 package com.domain.evernet.model;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 import androidx.annotation.NonNull;
 
-public class Contact {
+public class Contact implements Parcelable {
 
     private int id;
     private String name;
@@ -10,6 +13,11 @@ public class Contact {
     public Contact(int id, String name){
         this.id = id;
         this.name = name;
+    }
+
+    public Contact(Parcel in){
+        id = in.readInt();
+        name = in.readString();
     }
 
     public int getId() {
@@ -34,4 +42,27 @@ public class Contact {
         return new Contact(this.id, this.name);
 
     }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeInt(id);
+        dest.writeString(name);
+
+    }
+
+    public static final Parcelable.Creator<Contact> CREATOR = new Parcelable.Creator<Contact>() {
+        public Contact createFromParcel(Parcel in) {
+            return new Contact(in);
+        }
+
+        public Contact[] newArray(int size) {
+            return new Contact[size];
+        }
+    };
+
 }
